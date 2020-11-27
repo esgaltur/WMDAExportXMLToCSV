41b85f70b1e57f3e507bc5b7c3d9b7cd6414bf62
dmitriy@sosnovich.com <Dmitriy>
2020-11-27 01:45:03 +0100

    refactoring to consts

---
+++ uConsts.pas
@@ -0,0 +1,19 @@
+unit uConsts;
+
+interface
+const
+  cPythonInstalledDir = 'D:\Python39\';
+  cPythonDll = 'python39.dll';
+  cPythonDllFullPath = cPythonInstalledDir + cPythonDll;
+  cOutFull = './out/wmda_xml_full.csv';
+  cDropped = './out/wmda_xml_%s%s.csv';
+  cPandasLib = 'pandas';
+  cElementTreeLib = 'xml.etree.ElementTree';
+  cOsLib =   'os';
+  cDateTimeLib = 'datetime';
+  cXPathStart =  './';
+  cPythonNewStringExpresstion = 'str(''\n'')';
+  cCSVDelimiter = ';';
+implementation
+
+end.
